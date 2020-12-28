0fd21e35b6520ea23f30623f4eb9d6010b581c6d
Shohei-Kaneko-0613 <38118687+Shohei-Kaneko-0613@users.noreply.github.com>
2020-12-28 17:21:22 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+variables "aws_access_key_id" {}
+variables "aws_secret_access_key" {}
+
+provider "aws" {
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  region = "ap-northeast-1"
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
